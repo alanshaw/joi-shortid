52720bb69fcb6a572c5e24da62063ef9d36e7852
Alan Shaw <alan@tableflip.io>
2016-02-08 13:12:01 +0000

    Joi is peer dependency

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Joi = require('joi')
 
 module.exports = function () {
-  return Joi.string().regex(/^[a-zA-Z0-9_-]{7,14}$/)
+  return Joi.string().regex(/^[a-zA-Z0-9_-]{7,14}$/).description('shortid')
 }
